97f5da17b9415dc5df364b6620587b7c57cdac92
Billy <billy.m.nightingale@gmail.com>
2022-07-21 13:53:16 +0200

    set-up a fetch request within useEffect hook that will make a GET request to the backend for the users trip data

--- src/pages/SurfLog.jsx
+++ src/pages/SurfLog.jsx
@@ -1,11 +1,31 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { MyContext } from '../context/MyProvider';
 import NavBar from "../components/NavBar";
 
 
 const SurfLog = () => {
+    const context = useContext(MyContext);
+
+    let [ trips, setTrips ] = useState();
+
+    let id = context.userId;
+
+    useEffect(() => {
+        fetch(`http://localhost:3306/api/trips/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+            setTrips(data);
+            console.log(data);
+        });
+    }, [id]);
 
     return (
         <div>
             <NavBar />
+            {trips ? 
+            <div className="log-page-trip-table">
+                <p>Data Loaded: This is where the trips will go</p>
+            </div> :  <p>Sorry you have no trips to display</p>}
             <p>The surf log</p>
         </div>
     );
